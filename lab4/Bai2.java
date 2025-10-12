6c0e4a2d495e79e4835afbd63e822f91b87f63e9
manhcuong <cuonglu603@gmail.com>
2025-10-12 12:17:20 +0700

    'lab4'

---
+++ lab4/Bai2.java
@@ -0,0 +1,13 @@
+package com.aptect.lab4;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        SanPham sp1 = new SanPham(), sp2 = new SanPham();
+
+        sp1.nhap();
+        sp2.nhap();
+
+        sp1.xuat();
+        sp2.xuat();
+    }
+}
---
+++ lab4/Bai3.java
@@ -0,0 +1,8 @@
+package com.aptect.lab4;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        SanPham sp = new SanPham("san pham 1", 30);
+        sp.xuat();
+    }
+}
